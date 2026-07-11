e5bb5b570da7961f10013af15bae25a97ebaebfd
Mohamed-Khaled-pro <mohamed.khaled.pro1@gmail.com>
2026-07-11 20:28:01 +0300

    update backend

--- api/index.js
+++ api/index.js
@@ -1,102 +1,12 @@
-require("dotenv").config();
-
 const express = require("express");
 const serverless = require("serverless-http");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const cors = require("cors");
-const cookieParser = require("cookie-parser");
-const compression = require("compression");
 
 const app = express();
 
-
-// CORS
-app.use(
-  cors({
-    origin: [
-      "http://localhost:3000",
-      "http://localhost:5173",
-      "https://digitronix-frontend.vercel.app",
-    ],
-    credentials: true,
-  })
-);
-
-
-// Middlewares
-app.use(cookieParser());
-app.use(express.json());
-app.use(morgan("combined"));
-app.use(compression());
-
-
-// Logger
-app.use((req, res, next) => {
-  console.log(
-    `✅ ${req.method} ${req.url} - ${new Date().toISOString()}`
-  );
-  next();
-});
-
-
-// Test Route (قبل JWT)
 app.get("/", (req, res) => {
-  res.json({
-    message: "Hello from Digitronix Backend 🚀",
+  res.status(200).json({
+    message: "Digitronix Backend Working 🚀"
   });
 });
 
-
-// MongoDB Connection
-mongoose
-  .connect(process.env.CONNECTION_BASE, {
-    dbName: "mydatabase",
-    serverSelectionTimeoutMS: 5000,
-  })
-  .then(() => {
-    console.log("✅ Connected to database");
-  })
-  .catch((err) => {
-    console.log("❌ Database Error:", err.message);
-  });
-
-
-// Routes
-const productsRoutes = require("../routes/products");
-const usersRoutes = require("../routes/users");
-const ordersRoutes = require("../routes/orders");
-const categoriesRoutes = require("../routes/categories");
-
-const authJwt = require("../helpers/jwt");
-const errorHandler = require("../helpers/error-handler");
-
-
-// JWT
-app.use(authJwt());
-
-
-// API Routes
-app.use("/api/products", productsRoutes);
-app.use("/api/categories", categoriesRoutes);
-app.use("/api/users", usersRoutes);
-app.use("/api/orders", ordersRoutes);
-
-
-// Error Handler
-app.use(errorHandler);
-
-
-
-// Localhost Run
-if (process.env.NODE_ENV !== "production") {
-  const PORT = process.env.PORT || 5000;
-
-  app.listen(PORT, () => {
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
-  });
-}
-
-
-// Vercel Export
 module.exports = serverless(app);
